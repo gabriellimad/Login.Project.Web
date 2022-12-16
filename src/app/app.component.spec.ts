edde3a28ae5310fb8776ead4193b32f751a96eb1
gabriellimad <94320456+gabriellimad@users.noreply.github.com>
2022-12-15 21:55:36 -0300

    Addition of project's dependencys

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -20,16 +20,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'Login.Project.Web'`, () => {
+  it(`should have as title 'login'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('Login.Project.Web');
+    expect(app.title).toEqual('login');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('.content span')?.textContent).toContain('Login.Project.Web app is running!');
+    expect(compiled.querySelector('.content span')?.textContent).toContain('login app is running!');
   });
 });
